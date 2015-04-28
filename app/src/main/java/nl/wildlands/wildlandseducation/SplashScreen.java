726f3a3e0f96c26f42ca789b613665c863ed2ca6
StefanKoopman <stefan-koopman@hotmail.com>
2015-04-28 13:59:46 +0200

    Screen 1 toegevoegd

--- app/src/main/java/nl/wildlands/wildlandseducation/SplashScreen.java
+++ app/src/main/java/nl/wildlands/wildlandseducation/SplashScreen.java
@@ -1,6 +1,9 @@
 package nl.wildlands.wildlandseducation;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,13 +11,29 @@ import android.view.MenuItem;
 
 
 public class SplashScreen extends Activity {
+    private static final int DISPLAY_DATA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mHandler.sendEmptyMessageDelayed(DISPLAY_DATA, 1500);
     }
 
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            // Do task here
+            if (msg.what == DISPLAY_DATA) startKaart();
+        }
+    };
+
+    public void startKaart()
+    {
+        Intent h = new Intent(this, Kaart.class);
+        startActivity(h);
+        this.finish();
+    }
 
 
 }
