3ef31b2bb6465d799cd1b2986e226a40bbaf28b8
StefanKoopman <stefan-koopman@hotmail.com>
2015-04-28 15:00:48 +0200

    Launcher icons

--- app/src/main/java/nl/wildlands/wildlandseducation/SplashScreen.java
+++ app/src/main/java/nl/wildlands/wildlandseducation/SplashScreen.java
@@ -4,30 +4,38 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
+/*
+Splashscreen voor het begin van de app
+Weergave van een x aantal seconden
+ */
 
 public class SplashScreen extends Activity {
-    private static final int DISPLAY_DATA = 1;
+    private static final int MSDELAY = 1500;            // Aantal ms voor het doorschakelen naar contentweergave
+    private static final int DISPLAY_DATA = 1;          // Checkwaarde voor Handler
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        mHandler.sendEmptyMessageDelayed(DISPLAY_DATA, 1500);
+        mHandler.sendEmptyMessageDelayed(DISPLAY_DATA, MSDELAY);
     }
 
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            // Do task here
-            if (msg.what == DISPLAY_DATA) startKaart();
+
+            if (msg.what == DISPLAY_DATA)
+            {
+                startKaart();                       // Roep functie aan na splashtijd
+            }
         }
     };
 
+    /**
+     * Functie die de kaart activiteit start
+     */
     public void startKaart()
     {
         Intent h = new Intent(this, Kaart.class);
